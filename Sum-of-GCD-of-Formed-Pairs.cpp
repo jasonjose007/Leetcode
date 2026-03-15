43257a2541df28bab20af02b151df3f56ffcf2ea
Jason <jasonjose006@gmail.com>
2026-03-15 20:24:45 +0530

    [3867] [Time Beats: 50.00%] [Memory Beats: 12.50%] - LeetPush

--- Sum-of-GCD-of-Formed-Pairs.cpp
+++ Sum-of-GCD-of-Formed-Pairs.cpp
@@ -1,22 +1,21 @@
 1class Solution {
 2public:
 3    long long gcdSum(vector<int>& nums) {
-4        vector<int> velqoradin = nums;
-5        int n = velqoradin.size();
-6        vector<int> prefixGcd(n);
-7        int mx = velqoradin[0];
-8        for(int i = 0; i < n; i++){
-9            mx = max(mx, velqoradin[i]);
-10            prefixGcd[i] = __gcd(velqoradin[i], mx);
-11        }
-12        sort(prefixGcd.begin(), prefixGcd.end());
-13        long long ans = 0;
-14        int l = 0, r = n - 1;
-15        while(l < r){
-16            ans += __gcd(prefixGcd[l], prefixGcd[r]);
-17            l++;
-18            r--;
-19        }
-20        return ans;
-21    }
-22};
+4        vector<int>pgcd(nums.size());
+5        int maxi=nums[0];
+6        for(int i=0;i<nums.size();i++){
+7            if(nums[i]>maxi) maxi=nums[i];
+8            pgcd[i]= __gcd(maxi,nums[i]);
+9            
+10        }
+11        sort(pgcd.begin(),pgcd.end());
+12        long long int sum=0;
+13        int i=0,j=nums.size()-1;
+14        while(i<j){
+15            sum+=__gcd(pgcd[i],pgcd[j]);
+16            i++;
+17            j--;
+18        }
+19        return sum;
+20    }
+21};
