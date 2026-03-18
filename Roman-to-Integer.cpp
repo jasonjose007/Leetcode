f8de7c4d9634e98373733ed5a5d865f7bcfd84ac
Jason <jasonjose006@gmail.com>
2026-03-18 20:34:11 +0530

    [13] [Time Beats: 41.08%] [Memory Beats: 76.77%] - LeetPush

--- Roman-to-Integer.cpp
+++ Roman-to-Integer.cpp
@@ -1,36 +1,32 @@
 1class Solution {
-2    public int romanToInt(String s) {
-3        int ans = 0, num = 0;
-4        for (int i = s.length() - 1; i >= 0; i--) {
-5            switch (s.charAt(i)) {
-6                case 'I':
-7                    num = 1;
-8                    break;
-9                case 'V':
-10                    num = 5;
-11                    break;
-12                case 'X':
-13                    num = 10;
-14                    break;
-15                case 'L':
-16                    num = 50;
-17                    break;
-18                case 'C':
-19                    num = 100;
-20                    break;
-21                case 'D':
-22                    num = 500;
-23                    break;
-24                case 'M':
-25                    num = 1000;
-26                    break;
-27            }
-28            if (4 * num < ans)
-29                ans -= num;
-30            else
-31                ans += num;
-32        }
-33        return ans;
-34
-35    }
-36}
+2public:
+3
+4    int charToNum( char a ){
+5        switch (a) {
+6            case 'I' : return 1 ;
+7            case 'V' : return 5 ;
+8            case 'X' : return 10 ;
+9            case 'L' : return 50;
+10            case 'C' : return 100;
+11            case 'D' : return 500;
+12            case 'M' : return 1000;
+13            default : return 0 ;
+14        }
+15    }
+16
+17    int romanToInt(string s) {
+18        int n = s.size() ;
+19        int res = 0 ;
+20        for ( int i =0; i < n ; i++){
+21             if (i + 1 < n && charToNum(s[i]) < charToNum(s[i+1]) ){
+22                res -= charToNum(s[i]) ;
+23
+24             }else {
+25                res += charToNum(s[i]) ;
+26             }
+27
+28        }
+29        return res ;
+30        
+31    }
+32};
