313f8570fdd2947fab3a76d799fc551d3592708a
Jason <jasonjose006@gmail.com>
2026-08-11 18:37:36 +0530

    LC-smallest-missing-integer-greater-than-sequential-prefix-sum [Runtime: N/A; Memory: 19.3]

---
+++ array/smallest-missing-integer-greater-than-sequential-prefix-sum.py
@@ -0,0 +1,17 @@
+# Smallest Missing Integer Greater Than Sequential Prefix Sum
+# Difficulty: Easy
+# Topics: Array, Hash Table, Sorting
+
+class Solution:
+    def missingInteger(self, nums: List[int]) -> int:
+        i = 1
+        while i < len(nums) and nums[i] == nums[i - 1] + 1:
+            i += 1
+        
+        s = sum(nums[:i])
+        num_set = set(nums)
+        
+        while s in num_set:
+            s += 1
+            
+        return s
