374462ffe67a87905d3b0e3a6f7ac1792f331d92
Jason <jasonjose006@gmail.com>
2026-08-14 18:33:07 +0530

    LC-maximum-length-substring-with-two-occurrences [Runtime: N/A; Memory: 19.2]

--- Maximum-Length-Substring-With-Two-Occurrences.py
+++ Maximum-Length-Substring-With-Two-Occurrences.py
@@ -4,14 +4,14 @@
 
 class Solution:
     def maximumLengthSubstring(self, s: str) -> int:
-        char_counts = {}
+        counts = {}
         left = 0
         max_len = 0
         for right in range(len(s)):
             char = s[right]
-            char_counts[char] = char_counts.get(char, 0) + 1
-            while char_counts[char] > 2:
-                char_counts[s[left]] -= 1
+            counts[char] = counts.get(char, 0) + 1
+            while counts[char] > 2:
+                counts[s[left]] -= 1
                 left += 1
             max_len = max(max_len, right - left + 1)
         return max_len
