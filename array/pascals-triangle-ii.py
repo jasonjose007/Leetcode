1035a6da610fa26a6b6ce28984b6e904f2c03d27
Jason <jasonjose006@gmail.com>
2026-08-18 00:20:35 +0530

    LC-pascals-triangle-ii [Runtime: N/A; Memory: 19.2]

---
+++ array/pascals-triangle-ii.py
@@ -0,0 +1,10 @@
+# Pascal's Triangle II
+# Difficulty: Easy
+# Topics: Array, Dynamic Programming
+
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        row = [1]
+        for i in range(1, rowIndex + 1):
+            row.append(row[-1] * (rowIndex - i + 1) // i)
+        return row
