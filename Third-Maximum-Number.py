9a6e7a9018eb32600762dc0db41b12a3232234a2
Jason <jasonjose006@gmail.com>
2026-08-21 18:19:53 +0530

    LC-third-maximum-number [Runtime: 3; Memory: 19.7]

---
+++ Third-Maximum-Number.py
@@ -0,0 +1,12 @@
+# Third Maximum Number
+# Difficulty: Easy
+# Topics: Array, Sorting
+
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        s = set()
+        for num in nums:
+            s.add(num)
+            if len(s) > 3:
+                s.remove(min(s))
+        return min(s) if len(s) == 3 else max(s)
