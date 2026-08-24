77645ff43f90d28e06e9f4242d05cc6ab26ed9d2
Jason <jasonjose006@gmail.com>
2026-08-24 18:21:51 +0530

    LC-latest-time-by-replacing-hidden-digits [Runtime: N/A; Memory: 19.3]

---
+++ string/latest-time-by-replacing-hidden-digits.py
@@ -0,0 +1,26 @@
+# Latest Time by Replacing Hidden Digits
+# Difficulty: Easy
+# Topics: String, Greedy
+
+class Solution:
+    def maximumTime(self, time: str) -> str:
+        t = list(time)
+        if t[0] == '?':
+            if t[1] == '?' or t[1] < '4':
+                t[0] = '2'
+            else:
+                t[0] = '1'
+        
+        if t[1] == '?':
+            if t[0] == '2':
+                t[1] = '3'
+            else:
+                t[1] = '9'
+                
+        if t[3] == '?':
+            t[3] = '5'
+            
+        if t[4] == '?':
+            t[4] = '9'
+            
+        return "".join(t)
