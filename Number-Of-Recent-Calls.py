1737068c66ac3abeb5ad54e2ff984e01fe7880e8
Jason <jasonjose006@gmail.com>
2026-09-01 00:20:58 +0530

    LC-number-of-recent-calls [Runtime: 48; Memory: 24.2]

---
+++ Number-Of-Recent-Calls.py
@@ -0,0 +1,16 @@
+# Number of Recent Calls
+# Difficulty: Easy
+# Topics: Design, Queue, Data Stream
+
+from collections import deque
+
+class RecentCounter:
+
+    def __init__(self):
+        self.requests = deque()
+
+    def ping(self, t: int) -> int:
+        self.requests.append(t)
+        while self.requests[0] < t - 3000:
+            self.requests.popleft()
+        return len(self.requests)
