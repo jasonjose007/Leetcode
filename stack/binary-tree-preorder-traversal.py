c6d977d00915c5d45f070e0b1928becb0b0bf6df
Jason <jasonjose006@gmail.com>
2026-09-04 21:27:20 +0530

    LC-binary-tree-preorder-traversal [Runtime: N/A; Memory: 19.2]

---
+++ stack/binary-tree-preorder-traversal.py
@@ -0,0 +1,21 @@
+# Binary Tree Preorder Traversal
+# Difficulty: Easy
+# Topics: Stack, Tree, Depth-First Search, Binary Tree
+
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        
+        result = []
+        stack = [root]
+        
+        while stack:
+            node = stack.pop()
+            result.append(node.val)
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+                
+        return result
