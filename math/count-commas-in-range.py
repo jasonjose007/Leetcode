364a89e1aaa85feeba0014f7f0c9ef19648281b7
Jason <jasonjose006@gmail.com>
2026-09-08 21:31:07 +0530

    LC-count-commas-in-range [Runtime: N/A; Memory: 19.2]

---
+++ math/count-commas-in-range.py
@@ -0,0 +1,12 @@
+# Count Commas in Range
+# Difficulty: Easy
+# Topics: Math
+
+class Solution:
+    def countCommas(self, n: int) -> int:
+        ans = 0
+        threshold = 1000
+        while n >= threshold:
+            ans += n - threshold + 1
+            threshold *= 1000
+        return ans
