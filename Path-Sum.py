cbcb712af38845e94fac3643fd94930b7539c958
Jason <jasonjose006@gmail.com>
2026-09-11 21:27:52 +0530

    LC-path-sum [Runtime: N/A; Memory: 20.2]

---
+++ Path-Sum.py
@@ -0,0 +1,14 @@
+# Path Sum
+# Difficulty: Easy
+# Topics: Tree, Depth-First Search, Breadth-First Search, Binary Tree
+
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if not root:
+            return False
+        
+        if not root.left and not root.right:
+            return root.val == targetSum
+        
+        remaining_sum = targetSum - root.val
+        return self.hasPathSum(root.left, remaining_sum) or self.hasPathSum(root.right, remaining_sum)
