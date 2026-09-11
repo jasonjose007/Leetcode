6748169ed75fe8c5e0aef793d8ca999bb9b7bca2
Jason <jasonjose006@gmail.com>
2026-09-11 21:31:07 +0530

    LC-implement-stack-using-queues [Runtime: N/A; Memory: 19.4]

---
+++ Implement-Stack-Using-Queues.py
@@ -0,0 +1,25 @@
+# Implement Stack using Queues
+# Difficulty: Easy
+# Topics: Stack, Design, Queue
+
+from collections import deque
+
+class MyStack:
+
+    def __init__(self):
+        self.q = deque()
+
+    def push(self, x: int) -> None:
+        n = len(self.q)
+        self.q.append(x)
+        for _ in range(n):
+            self.q.append(self.q.popleft())
+
+    def pop(self) -> int:
+        return self.q.popleft()
+
+    def top(self) -> int:
+        return self.q[0]
+
+    def empty(self) -> bool:
+        return len(self.q) == 0
