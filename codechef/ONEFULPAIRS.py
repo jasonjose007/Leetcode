2634586af5f208e8515ee9a19944024b50a6c683
Jason <jasonjose006@gmail.com>
2026-09-14 21:42:04 +0530

    CC-ONEFULPAIRS [backfill]

---
+++ codechef/ONEFULPAIRS.py
@@ -0,0 +1,19 @@
+# Oneful Pairs
+# Platform: CodeChef
+# Difficulty: Easy
+
+import sys
+
+def solve():
+    input_data = sys.stdin.read().split()
+    if not input_data:
+        return
+    a = int(input_data[0])
+    b = int(input_data[1])
+    if a + b + a * b == 111:
+        print("Yes")
+    else:
+        print("No")
+
+if __name__ == '__main__':
+    solve()
