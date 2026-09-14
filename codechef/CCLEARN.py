f5f0d47b772afeedb4474ab45c63e5a3215eea8f
Jason <jasonjose006@gmail.com>
2026-09-14 21:41:47 +0530

    CC-CCLEARN [backfill]

---
+++ codechef/CCLEARN.py
@@ -0,0 +1,14 @@
+# CodeChef Learn Problem Solving
+# Platform: CodeChef
+# Difficulty: Easy
+
+import sys
+
+def main():
+    input_data = sys.stdin.read().split()
+    if input_data:
+        n = int(input_data[0])
+        print(2 * n)
+
+if __name__ == '__main__':
+    main()
