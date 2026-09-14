53952ce7665b4f338c65620abcb8687b3f9ef64d
Jason <jasonjose006@gmail.com>
2026-09-14 21:41:39 +0530

    CC-START01 [backfill]

---
+++ codechef/START01.py
@@ -0,0 +1,13 @@
+# Number Mirror
+# Platform: CodeChef
+# Difficulty: Easy
+
+import sys
+
+def main():
+    input_data = sys.stdin.read().split()
+    if input_data:
+        print(input_data[0])
+
+if __name__ == '__main__':
+    main()
