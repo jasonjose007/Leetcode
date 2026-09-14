f241a84f14704779aa5f83c85794c11afa5dfb24
Jason <jasonjose006@gmail.com>
2026-09-14 12:50:27 +0530

    LC-reverse-bits [Runtime: 45; Memory: 19.2]

---
+++ divide-and-conquer/reverse-bits.py
@@ -0,0 +1,11 @@
+# Reverse Bits
+# Difficulty: Easy
+# Topics: Divide and Conquer, Bit Manipulation
+
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        ans = 0
+        for _ in range(32):
+            ans = (ans << 1) | (n & 1)
+            n >>= 1
+        return ans
