cf9e498e1d1959aa5b1316f8daebcd3ad74a2e1c
Jason <jasonjose006@gmail.com>
2026-09-15 13:24:37 +0530

    LC-alternating-groups-i [Runtime: N/A; Memory: N/A]

---
+++ sliding-window/alternating-groups-i.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numberOfAlternatingGroups(self, colors: List[int]) -> int:
+        n=len(colors)
+        count=0
+        if n ==0:
+            return 0
+        for i in range(1,n-1):
+            if colors[i] !=colors[i-1] and colors[i]!=colors[i+1]:
+                count +=1
+        if colors[0] != colors[1] and colors [0]!=colors[-1]:
+            count +=1
+        if colors[-2]!=colors[-1] and colors [-1]!=colors[0]:
+            count +=1
+        return count
